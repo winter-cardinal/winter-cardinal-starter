5c663f83969871232ed3e1e96f1d0313a64ba43f
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2024-09-18 20:28:19 +0900

    rename: MvcGraphicController to MvcGraphicEditorController

--- src/main/java/app/mvc/MvcGraphicEditorController.java
+++ src/main/java/app/mvc/MvcGraphicEditorController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MvcGraphicController {
+public class MvcGraphicEditorController {
 	@GetMapping("/graphic-editor")
 	public String main() {
 		return "graphic-editor/main";
